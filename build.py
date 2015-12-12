13f857978378df38a4d52cc92aec750c3804f069
Logan Walker <logan.walker@me.com>
2015-12-12 13:17:34 -0500

    Remove need for static pages

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
 # OSU SPS Website Build Script
 
+import os
+
 def loadFile( src, prefx ):
     out = ""
     for line in open( src, 'r' ):
@@ -21,9 +23,11 @@ header = header.replace( "[SCRIPT]", loadFile( includes_folder + "script.js", "\
 
 footer = loadFile( includes_folder + "footer.html", "" )
 
-names = [
-    "index.html"
-]
+names = set()
+for name in os.listdir( src_folder ):
+    names.add( name )
+    
+names.remove( includes_folder[ len( src_folder ) : -1 ] )
 
 for name in names:
     raw = loadFile( src_folder + name, "" )
