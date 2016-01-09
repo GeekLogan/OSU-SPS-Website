57a6af8e0447350e2242b751ef6a029b863538a5
Logan Walker <logan.walker@me.com>
2016-01-09 17:58:22 -0500

    Reduce File Count

--- build.py
+++ build.py
@@ -17,12 +17,6 @@ out_folder = "output/"
 src_folder = "src/"
 includes_folder = src_folder + "includes/"
 
-header = loadFile( includes_folder + "header.html", "" )
-header = header.replace( "[STYLE]", loadFile( includes_folder + "style.css", "\t" ) )
-header = header.replace( "[SCRIPT]", loadFile( includes_folder + "script.js", "\t" ) )
-
-footer = loadFile( includes_folder + "footer.html", "" )
-
 names = set()
 for name in os.listdir( src_folder ):
     names.add( name )
@@ -31,5 +25,5 @@ names.remove( includes_folder[ len( src_folder ) : -1 ] )
 
 for name in names:
     raw = loadFile( src_folder + name, "" )
-    raw = header + raw + footer
+    raw = raw.replace( "[STYLE]", loadFile( includes_folder + "style.css", "\t" ) )
     outputFile( out_folder + name, raw )
